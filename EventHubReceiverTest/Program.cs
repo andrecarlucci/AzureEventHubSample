ac5588f5ff4c584bb6ca0d2e51e9d43fbbcce760
andrecarlucci <andrecarlucci@gmail.com>
2018-01-17 17:48:33 -0200

    initial

---
+++ EventHubReceiverTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubReceiverTest {
+    partial class Program {
+
+        private const string _connectionString = "foo";
+        private static bool _receive = true;
+
+        static async Task Main(string[] args) {
+            Console.WriteLine("Start!");
+
+            var client = EventHubClient.CreateFromConnectionString(_connectionString);
+            var info = await client.GetRuntimeInformationAsync();
+
+            var receiver = client.CreateReceiver(PartitionReceiver.DefaultConsumerGroupName,
+                                                 info.PartitionIds[0],
+                                                 "");
+
+
+            receiver.SetReceiveHandler(new PartitionReceiveHandler());
+
+            Task.Factory.StartNew(async () => {
+                while (_receive) {
+                    Console.WriteLine("Waiting");
+                    var msgs = await receiver.ReceiveAsync(200);
+                    msgs.ConsoleWrite();
+                }
+            });
+            
+            Console.WriteLine("Press any key to stop");
+            Console.ReadLine();
+            await Stop(client, receiver);
+        }
+
+        private static async Task Stop(EventHubClient client, PartitionReceiver receiver) {
+            _receive = false;
+            Console.WriteLine("Stoping...");
+            await receiver.CloseAsync();
+            await client.CloseAsync();
+            Console.WriteLine("The end");
+        }
+    }
+
+    public class PartitionReceiveHandler : IPartitionReceiveHandler {
+        public int MaxBatchSize { get; }
+        
+        public Task ProcessEventsAsync(IEnumerable<EventData> msgs) {
+            if(msgs == null) {
+                Console.WriteLine("Null :(");
+                return Task.CompletedTask;
+            }
+            msgs.ConsoleWrite();
+            return Task.CompletedTask;
+        }
+        public Task ProcessErrorAsync(Exception error) {
+            Console.WriteLine("Error!: " + error);
+            return Task.CompletedTask;
+        }
+    }
+
+    public static class EventDataExtensions {
+        public static void ConsoleWrite(this IEnumerable<EventData> msgs) {
+            foreach (var msg in msgs) {
+                var text = Encoding.UTF8.GetString(msg.Body.ToArray());
+                Console.WriteLine($"Message: Seq: {msg.SystemProperties.SequenceNumber} OffSet: {msg.SystemProperties.Offset} Text: {text}");
+            }
+        }
+    }
+}
--- EventHubTest/Program.cs
+++ EventHubTest/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace EventHubTest
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace EventHubTest {
+    class Program {
+        private const string _connectionString = "Endpoint=sb://powerhub-event-hub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=iOBuwz43glBQ0rOhd9I1kC1o4k2ot6HLqKQ8C+Op+4c=;EntityPath=powerhub-timeseries";
+
+        private static EventHubClient _eventHubClient;
+        
+        static async Task Main(string[] args) {
+            _eventHubClient = EventHubClient.CreateFromConnectionString(_connectionString);
+
+            await SendMessagesToEventHub(100);
+            await _eventHubClient.CloseAsync();
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        // Creates an event hub client and sends 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend) {
+            for (var i = 0; i < numMessagesToSend; i++) {
+                try {
+                    var message = $"Message {i} {DateTime.Now} ";
+                    Console.WriteLine($"Sending message: {message}");
+                    await _eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception) {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
         }
     }
 }
